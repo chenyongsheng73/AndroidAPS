7ba3cf666bc6d33c6bf0c2d519df409bbf0fcc9a
chenyongsheng73 <44947075+chenyongsheng73@users.noreply.github.com>
2024-09-20 15:41:17 +0800

    Update XdripSourcePlugin.kt4

--- plugins/source/src/main/kotlin/app/aaps/plugins/source/XdripSourcePlugin.kt
+++ plugins/source/src/main/kotlin/app/aaps/plugins/source/XdripSourcePlugin.kt
@@ -59,9 +59,6 @@ class XdripSourcePlugin @Inject constructor(
             GlucoseValue.SourceSensor.DEXCOM_NATIVE_UNKNOWN,
             GlucoseValue.SourceSensor.DEXCOM_G6_NATIVE,
             GlucoseValue.SourceSensor.DEXCOM_G5_NATIVE,
-            GlucoseValue.SourceSensor.DEXCOM_G5_NATIVE_XDRIP,
-            GlucoseValue.SourceSensor.DEXCOM_G6_G5_NATIVE_XDRIP,
-        
         ).any { it == glucoseValue.sourceSensor }
     }
     
